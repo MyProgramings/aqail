5680b3b00dd5ab327504beb74da92bf41b933a63
Abdullah Baagail <reader1999a@gmail.com>
2026-08-10 06:24:37 +0300

    sss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
       'Abdullah Baaqail - Software designer, founder, and amateur astronaut',
   },
   description:
-    'I’m Abdullah, a software designer and entrepreneur based in Riyadh City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I`m Abdullah, a UI/UX designer based in Riyadh. I develop solutions that empower individuals to explore their digital world on their own terms.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
